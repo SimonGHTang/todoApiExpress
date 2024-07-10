313d911316acf0bdd16bdd18e21b82e441e1f982
Simon Tang <simon.gh.tang@gmail.com>
2024-07-10 18:02:36 +1000

    changed put to edit instead of add added CORS for request/response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const taskDatabaseRoute = require('./routes/task/task-database-route.js');
 const app = express();
 const port = 3000;
+const corOptions = { 
+	origin: "http://localhost:5173/",
+	methods: ['GET', 'PUT', 'POST', 'DELETE'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
 
+}
+
+app.use(cors(corOptions));
 app.use(express.json());
 app.use('/taskDatabase', taskDatabaseRoute);
 
--- src/routes/task/put-task.js
+++ src/routes/task/put-task.js
@@ -2,8 +2,8 @@ const taskService = require('../../service/task/task-service.js');
 
 const putTask = (req, res) => {
 	//need to verify req body
-	taskService.addTask(req.body);
-	res.status(200).send('task added');
+	taskService.editTask(req.body);
+	res.status(200).send('task edited');
 }
 
 module.exports = putTask;
